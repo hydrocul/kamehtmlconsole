9c98d2d0017a4d13c229c78c955c29e70e978b1b
hydrocul <hydrocul@gmail.com>
2010-11-19 00:37:33 +0900

    update ConsoleHandler.scala

--- src/hydrocul/kamehtmlconsole/ConsoleHandler.scala
+++ src/hydrocul/kamehtmlconsole/ConsoleHandler.scala
@@ -1,9 +1,36 @@
 package hydrocul.kamehtmlconsole;
 
-class ConsoleHandler {
+import java.io.PrintWriter;
 
-  def handle(path: String, request: HttpServletRequest, response: HttpServletResponse){
-    
+import hydrocul.util.ObjectPool;
+
+class ConsoleHandler(objectPool: ObjectPool){
+
+  trait Request {
+
+    def getParameter(name: String): String;
+
+  }
+
+  trait Response {
+
+    def setContentType(contentType: String);
+
+    def setStatus(statucCode: Int);
+
+    def getWriter(): PrintWriter;
+
+  }
+
+  def handle(path: String, request: Request, response: Response){
+    // path: request path, empty string or string which starts with "/"
+
+    if(path.startsWith("/etc/")){
+      handleEtc(path.substring(5), request, response);
+    } else if(path.startsWith("/i/")){
+    }
+
+
   }
 
 }

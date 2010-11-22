f24110898b10494975725ab02336ec4da0bcf9c0
hydrocul <hydrocul@gmail.com>
2010-11-23 03:04:38 +0900

    update Console.scala, ConsoleLineGroup.scala

--- src/hydrocul/kamehtmlconsole/Console.scala
+++ src/hydrocul/kamehtmlconsole/Console.scala
@@ -8,24 +8,70 @@ trait Console {
 
   def newLineGroup(): ConsoleLineGroup;
 
+  def newLineGroupBefore(after: ConsoleLineGroup): ConsoleLineGroup;
+
+  def newLineGroupAfter(before: ConsoleLineGroup): ConsoleLineGroup;
+
 }
 
 private[kamehtmlconsole] class ConsoleImpl(objectPool: ObjectPool, baseUrl: String) extends Console {
 
   import ConsoleImpl._;
 
+  @volatile private var groups: IndexedSeq[ConsoleLineGroupImpl] = Vector();
+
   def getLoadingHtml = "<img src=\"" + baseUrl + "etc/loading.gif\" width=\"16\" height=\"16\" border=\"0\" alt=\"loading\" />";
 
   def newLineGroup(): ConsoleLineGroup = {
-    val ret = new ConsoleLineGroupImpl(objectPool);
-    // TODO newLineGroup
-    return ret;
+    synchronized {
+      val ret = new ConsoleLineGroupImpl(objectPool, groupListener);
+      groups = groups :+ ret;
+      return ret;
+    }
   }
+
+  def newLineGroupBefore(after: ConsoleLineGroup): ConsoleLineGroup = {
+    synchronized {
+      val i = groups.indexOf(after);
+      if(i < 0){
+        return new ConsoleLineGroupImpl(objectPool, groupListener);
+      }
+      val ret = new ConsoleLineGroupImpl(objectPool, groupListener);
+      groups = (groups.take(i) :+ ret) ++ groups.drop(i);
+      ret;
+    }
+  }
+
+  def newLineGroupAfter(before: ConsoleLineGroup): ConsoleLineGroup = {
+    synchronized {
+      val i = groups.indexOf(before) + 1;
+      if(i <= 0){
+        return new ConsoleLineGroupImpl(objectPool, groupListener);
+      }
+      val ret = new ConsoleLineGroupImpl(objectPool, groupListener);
+      groups = (groups.take(i) :+ ret) ++ groups.drop(i);
+      ret;
+    }
+  }
+
+  private def limitLineCount(){
+    // TODO
+  }
+
+  private val groupListener = new ConsoleLineGroupImpl.Listener {
+    def limitLineCount(){
+      ConsoleImpl.this.limitLineCount();
+    }
+  }
+
+  private[kamehtmlconsole] def size: Int = groups.map(_.size).sum;
+
+  private[kamehtmlconsole] def getLines: IndexedSeq[ConsoleLine] = groups.flatMap(_.getLines);
 
 }
 
 private[kamehtmlconsole] object ConsoleImpl {
 
-  private val maxLineCount = 500;
+  val maxLineCount = 500;
 
 }
--- src/hydrocul/kamehtmlconsole/ConsoleLineGroup.scala
+++ src/hydrocul/kamehtmlconsole/ConsoleLineGroup.scala
@@ -6,18 +6,70 @@ trait ConsoleLineGroup {
 
   def newLine(): ConsoleLineBuffer;
 
+  def newLineBefore(after: ConsoleLineBuffer): ConsoleLineBuffer;
+
+  def newLineAfter(before: ConsoleLineBuffer): ConsoleLineBuffer;
+
 }
 
-private[kamehtmlconsole] class ConsoleLineGroupImpl(objectPool: ObjectPool) extends ConsoleLineGroup {
+private[kamehtmlconsole] class ConsoleLineGroupImpl(objectPool: ObjectPool,
+  listener: ConsoleLineGroupImpl.Listener) extends ConsoleLineGroup {
 
-  @volatile private var lines: IndexedSeq[ConsoleLineBuffer] = Vector[ConsoleLineBuffer]();
+  @volatile private var lines: IndexedSeq[ConsoleLineBuffer] = Vector();
 
   def newLine(): ConsoleLineBuffer = {
-    val ret = new ConsoleLineBufferImpl(objectPool);
-    lines = lines :+ ret;
-    ret;
+    synchronized {
+      val ret = new ConsoleLineBufferImpl(objectPool);
+      lines = lines :+ ret;
+      limitLineCount();
+      ret;
+    }
   }
+
+  def newLineBefore(after: ConsoleLineBuffer): ConsoleLineBuffer = {
+    synchronized {
+      val i = lines.indexOf(after);
+      if(i < 0){
+        return new ConsoleLineBufferImpl(objectPool);
+      }
+      val ret = new ConsoleLineBufferImpl(objectPool);
+      lines = (lines.take(i) :+ ret) ++ lines.drop(i);
+      limitLineCount();
+      ret;
+    }
+  }
+
+  def newLineAfter(before: ConsoleLineBuffer): ConsoleLineBuffer = {
+    synchronized {
+      val i = lines.indexOf(before) + 1;
+      if(i <= 0){
+        return new ConsoleLineBufferImpl(objectPool);
+      }
+      val ret = new ConsoleLineBufferImpl(objectPool);
+      lines = (lines.take(i) :+ ret) ++ lines.drop(i);
+      limitLineCount();
+      ret;
+    }
+  }
+
+  private def limitLineCount(){
+    val s = lines.size;
+    if(s > ConsoleImpl.maxLineCount){
+      lines = lines.drop(s - ConsoleImpl.maxLineCount);
+    }
+  }
+
+  private[kamehtmlconsole] def size = lines.size;
 
   private[kamehtmlconsole] def getLines: IndexedSeq[ConsoleLine] = lines.map(_.getConsoleLine);
 
 }
+
+private[kamehtmlconsole] object ConsoleLineGroupImpl {
+
+  trait Listener {
+    def limitLineCount();
+  }
+
+}
+

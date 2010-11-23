f25ce4adb4edd73164ec14523096a0a368347a3a
hydrocul <hydrocul@gmail.com>
2010-11-23 18:29:44 +0900

    create ConsoleLineBufferTest.scala

--- test/src/hydrocul/kamehtmlconsole/ConsoleLineBufferTest.scala
+++ test/src/hydrocul/kamehtmlconsole/ConsoleLineBufferTest.scala
@@ -5,7 +5,20 @@ import scala.testing.SUnit;
 object ConsoleLineBufferTest {
 
   def testCases: IndexedSeq[SUnit.Test] = {
-    Vector();
+    Vector(
+      testGetTextFromHtml("abc", "abc"),
+      testGetTextFromHtml("abc<a>def</a>", "abcdef"),
+      testGetTextFromHtml("abc<a>de<span class=\"aaa\">f</span></a>", "abcdef"),
+      testGetTextFromHtml("", "")
+    );
+  }
+
+  private def testGetTextFromHtml(html: String, expected: String): SUnit.Test = new SUnit.TestCase(html){
+    override def enableStackTrace = false;
+    def runTest(){
+      val actual = ConsoleLineBufferImpl.getTextFromHtml(html);
+      assertEquals(expected, actual);
+    }
   }
 
 }

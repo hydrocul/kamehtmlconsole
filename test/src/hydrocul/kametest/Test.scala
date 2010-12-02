d95557767c671413e0f5fbaa2f3ca9554ede3001
hydrocul <hydrocul@gmail.com>
2010-12-02 23:59:58 +0900

    creating kametest

--- test/src/hydrocul/kametest/Test.scala
+++ test/src/hydrocul/kametest/Test.scala
@@ -5,12 +5,19 @@ import java.io.StringWriter;
 
 object Test {
 
+  private var _successCount: Int = 0;
+  private var _errorCount: Int = 0;
   private var _errorExists: Boolean = false;
 
   def errorExists: Boolean = _errorExists;
 
+  def getSuccessCount = _successCount;
+
+  def getErrorCount = _errorCount;
+
   def assertEquals[A](name: String, expected: A, actual: A){
     if(actual==expected){
+      _successCount = _successCount + 1;
       return;
     }
     val msg = "[ERROR] %s: expedted: %s, actual: %s %s".format(name, expected, actual, {
@@ -20,6 +27,7 @@ object Test {
       sw.toString.split("\n")(2).trim;
     });
     println(msg);
+    _errorCount = _errorCount + 1;
     _errorExists = true;
   }
 

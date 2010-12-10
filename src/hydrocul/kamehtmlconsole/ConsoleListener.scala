298b6990b1aa3d1a4644695bb10905c325c2a024
hydrocul <hydrocul@gmail.com>
2010-12-10 23:20:25 +0900

    updating KeyEvent.scala

--- src/hydrocul/kamehtmlconsole/ConsoleListener.scala
+++ src/hydrocul/kamehtmlconsole/ConsoleListener.scala
@@ -2,10 +2,6 @@ package hydrocul.kamehtmlconsole;
 
 trait ConsoleListener {
 
-  def userText(count: Int, text: String);
-
-  def specialKey(count: Int, key: String);
-
-  def specialCommand(count: Int, command: String);
+  def input(event: KeyEvent);
 
 }
--- src/hydrocul/kamehtmlconsole/KeyEvent.scala
+++ src/hydrocul/kamehtmlconsole/KeyEvent.scala
@@ -5,11 +5,19 @@ trait KeyEvent;
 case class UserTextKeyEvent(text: String) extends KeyEvent {
 }
 
-case class SpecialKeyEvent(key: String) extends KeyEvent {
+case class SpecialKeyEvent(key: SpecialKeyEvent.SpecialKey,
+  alt: Boolean, ctrl: Boolean, shift: Boolean) extends KeyEvent {
 }
 
 object SpecialKeyEvent {
 
+  private val keys: Map[String, SpecialKey] = Map();
+
+  sealed trait SpecialKey;
+  object Tab extends SpecialKey; keys = keys + ("Tab" -> Tab);
+  object Enter extends SpecialKey; keys = keys + ("Enter" -> Enter);
+
+  def getSpecialKey(key: String): SpecialKey;
 
 }
 

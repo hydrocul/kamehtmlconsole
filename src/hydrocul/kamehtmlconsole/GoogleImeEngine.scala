6081f360ea5a901deb72e3b15ce656095e23a7e4
hydrocul <hydrocul@gmail.com>
2010-12-15 02:38:06 +0900

    update GoogleImeEngine.scala

--- src/hydrocul/kamehtmlconsole/GoogleImeEngine.scala
+++ src/hydrocul/kamehtmlconsole/GoogleImeEngine.scala
@@ -9,27 +9,36 @@ import scala.io.Source;
 import net.arnx.jsonic.JSON;
 
 import hydrocul.kameq.scala.Pipe.ioexec;
+import hydrocul.util.RomaKanaHenkan;
 import hydrocul.util.StringLib;
 
 object GoogleImeEngine extends ImeEngine {
 
   def searchSuggestions(text: String, listener: ImeEngineListener){
 
-    val text2 = text; // TODO
+    val roma = extractRoma(text);
+    val hiragana = RomaKanaHenkan.roma2kana(roma);
 
     ioexec {
-      accessGoogleAPI(text2);
+      accessGoogleAPI(hiragana);
     } | { result: List[APIResponseItem] =>
-      null; // TODO
+      val r = result.last;
+      val len = r.word.length;
+      val suggestions: List[ImeEngineSuggestion] = r.kanji.map(kanji =>
+        new StdImeEngineSuggestion(kanji, len));
+      listener.receiveSuggestions(suggestions);
     }
 
   }
 
   private[kamehtmlconsole] def extractRoma(text: String): String = {
-    "" // TODO extractRoma
+    text match {
+      case romaPattern(roma) => roma;
+      case _ => "";
+    }
   }
 
-  private val romaPattern = "\\A.*?([a-z][-a-z])\\z".r;
+  private val romaPattern = "\\A.*?([a-z][-a-z]*)\\z".r;
 
   private[kamehtmlconsole] def accessGoogleAPI(text: String): List[APIResponseItem] = {
 
--- test/src/hydrocul/kamehtmlconsole/GoogleImeEngineTest.scala
+++ test/src/hydrocul/kamehtmlconsole/GoogleImeEngineTest.scala
@@ -5,11 +5,25 @@ import hydrocul.kametest.Test;
 object GoogleImeEngineTest {
 
   def test(){
-    testSub("にほんご", "にほんご", "日本語");
-    testSub("ここではきものを", "きものを", "着物を");
+    testExtractRoma("aiueo", "aiueo");
+    testExtractRoma("aa go-ka-to", "go-ka-to");
+    testExtractRoma("あいうえおaiueo", "aiueo");
+    testExtractRoma("aiueo ", "");
+    testExtractRoma("aiueo.", "");
+    testGoogleAPI("にほんご", "にほんご", "日本語");
+    testGoogleAPI("ここではきものを", "きものを", "着物を");
   }
 
-  private def testSub(text: String, text2: String, expected: String){
+  private def testSearchSuggestions(text: String, expected: String){
+    
+  }
+
+  private def testExtractRoma(text: String, roma: String){
+    val actual = GoogleImeEngine.extractRoma(text);
+    Test.assertEquals(text, roma, actual);
+  }
+
+  private def testGoogleAPI(text: String, text2: String, expected: String){
     val result: List[GoogleImeEngine.APIResponseItem] = GoogleImeEngine.accessGoogleAPI(text);
     Test.assertEquals(text, text2, result.last.word);
     Test.assertEquals(text, expected, result.last.kanji.head);
